67d08292eb4f3a59b9442446a9150f309c1a8b6f
GutoAS <agostinhosoberano2@gmail.com>
2022-12-30 10:54:31 +0200

    update copyrigths

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import aboutimage from '../assets/about.png'
+import React from "react";
+import aboutimage from "../assets/about.png";
 
 const About = () => {
   return (
-    <div id='about'>
-        <div className='about-text'>
-            <h1>PROXIMOS PRATOS</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus consequatur repellat repudiandae amet quaerat a excepturi accusantium sapiente, fugit eius laborum ipsum laboriosam totam, commodi quae beatae labore consequuntur maiores.</p>
-            <button>Mais Informações</button>
-        </div>
-        <div className='about-image'>
-            <img src={aboutimage} alt="" />
-        </div>
+    <div id="about">
+      <div className="about-text">
+        <h1>PROXIMOS PRATOS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          consequatur repellat repudiandae amet quaerat a excepturi accusantium
+          sapiente, fugit eius laborum ipsum laboriosam totam, commodi quae
+          beatae labore consequuntur maiores.
+        </p>
+        <button>Mais Informações</button>
+      </div>
+      <div className="about-image">
+        <img src={aboutimage} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div id="contact">
-        <h1>Contacte-nos</h1>
-        <form action="" method='POST'>
-            <input type="text" name='fullName' placeholder='Nome Completo'/>
-            <input type="email" name='fullName' placeholder='Escreva seu email (opcional)'/>
-            <input type="text" name="number" placeholder='Escreva seu numero' required  />
-            <textarea name="message" placeholder='Escreva seu pedido aqui...' cols="30" rows="10"></textarea>
-            <input type="submit" value="Enviar"/>
-        </form>
+      <h1>Contacte-nos</h1>
+      <form action="" method="POST">
+        <input type="text" name="fullName" placeholder="Nome Completo" />
+        <input
+          type="email"
+          name="fullName"
+          placeholder="Escreva seu email (opcional)"
+        />
+        <input
+          type="text"
+          name="number"
+          placeholder="Escreva seu numero"
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Escreva seu pedido aqui..."
+          cols="30"
+          rows="10"
+        ></textarea>
+        <input type="submit" value="Enviar" />
+      </form>
+      <p> &copy; Copyright Agostinho Soberano 2022</p>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
